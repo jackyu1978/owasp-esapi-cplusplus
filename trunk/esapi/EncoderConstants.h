8e69f2caac9f6a9c1674b96ecf4db268f032b916
deige101@gmail.com <deige101@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-01 19:23:49 +0000

    finished EncoderConstants

--- trunk/esapi/EncoderConstants.h
+++ trunk/esapi/EncoderConstants.h
@@ -56,51 +56,37 @@ public:
 	/**
 	 * CHAR_LETTERS union CHAR_DIGITS
 	 */
-	public final static char[] CHAR_ALPHANUMERICS = StringUtilities.union(CHAR_LETTERS, CHAR_DIGITS);
-	public final static Set<Character> ALPHANUMERICS;
-	static {
-		ALPHANUMERICS = CollectionsUtil.arrayToSet(CHAR_ALPHANUMERICS);
-	}
+	static const char CHAR_ALPHANUMERICS[];
+	static const std::set<char> ALPHANUMERICS;
 
 	/**
 	 * Password character set, is alphanumerics (without l, i, I, o, O, and 0)
 	 * selected specials like + (bad for URL encoding, | is like i and 1,
 	 * etc...)
 	 */
-	public final static char[] CHAR_PASSWORD_LOWERS = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-	public final static Set<Character> PASSWORD_LOWERS;
-	static {
-		PASSWORD_LOWERS = CollectionsUtil.arrayToSet(CHAR_ALPHANUMERICS);
-	}
+	static const char CHAR_PASSWORD_LOWERS[];
+	static const std::set<char> PASSWORD_LOWERS;
 
 	/**
 	 *
 	 */
-	public final static char[] CHAR_PASSWORD_UPPERS = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-	public final static Set<Character> PASSWORD_UPPERS;
-	static {
-		PASSWORD_UPPERS = CollectionsUtil.arrayToSet(CHAR_PASSWORD_UPPERS);
-	}
+	static const char CHAR_PASSWORD_UPPERS[];
+	static const std::set<char> PASSWORD_UPPERS;
 
 	/**
 	 * 2-9
 	 */
-	public final static char[] CHAR_PASSWORD_DIGITS = { '2', '3', '4', '5', '6', '7', '8', '9' };
-	public final static Set<Character> PASSWORD_DIGITS;
-	static {
-		PASSWORD_DIGITS = CollectionsUtil.arrayToSet(CHAR_PASSWORD_DIGITS);
-	}
+	static const char CHAR_PASSWORD_DIGITS[];
+	static const std::set<char> PASSWORD_DIGITS;
 
 	/**
 	 * CHAR_PASSWORD_LOWERS union CHAR_PASSWORD_UPPERS
 	 */
-	public final static char[] CHAR_PASSWORD_LETTERS = StringUtilities.union( CHAR_PASSWORD_LOWERS, CHAR_PASSWORD_UPPERS );
-	public final static Set<Character> PASSWORD_LETTERS;
-	static {
-		PASSWORD_LETTERS = CollectionsUtil.arrayToSet(CHAR_PASSWORD_LETTERS);
-	}
+	static const char CHAR_PASSWORD_LETTERS[];
+	static const std::set<char> PASSWORD_LETTERS;
 
-	private EncoderConstants() {
+private:
+	EncoderConstants() {
 		// prevent instantiation
 	}
 
