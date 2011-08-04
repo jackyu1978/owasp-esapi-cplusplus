f3facb7160b3506559e1f42def80a0a02d48ff28
noloader <noloader@gmail.com>
2011-08-04 04:25:52 +0000

    Added #pragma once Renamed  ->  (consistency) Changed ctor to use initialization rather than assignment Added initialization to varTemp and varMark varIndex and varMark from unsigned int to size_t Made class members varTemp and varPushback mutable (conceptually, a String is not changed when marked) Constness on parameters and class methods

--- src/codecs/PushbackString.cpp
+++ src/codecs/PushbackString.cpp
@@ -14,23 +14,25 @@
  * @created 2007
  */
 
+#include "EsapiCommon.h"
 #include "codecs/PushbackString.h"
 
-esapi::PushbackString::PushbackString( std::string input) {
-	this->input = input;
-	this->varIndex = 0;
-	this->varMark = 0;
+esapi::PushbackString::PushbackString(const std::string& input)
+	: input(input), varPushback(0), varTemp(0), varIndex(0), varMark(0) {
+	ASSERT(!input.empty());
 }
 
 void esapi::PushbackString::pushback( char c ){
+	ASSERT(c != 0);
 	this->varPushback = c;
 }
 
-int esapi::PushbackString::index(){
+size_t esapi::PushbackString::index() const{
+	ASSERT(varIndex < input.length());
 	return this->varIndex;
 }
 
-bool esapi::PushbackString::hasNext(){
+bool esapi::PushbackString::hasNext() const{
 	if ( this->varPushback != 0 ) return true;
 	if ( this->input.compare("") ) return false;
 	if ( this->input.length() == 0 ) return false;
@@ -47,6 +49,8 @@ char esapi::PushbackString::next() {
 	if ( this->input.compare("") ) return 0;
 	if ( this->input.length() == 0 ) return 0;
 	if ( this->varIndex >= this->input.length() ) return 0;
+
+	ASSERT(varIndex < input.length());
 	return this->input[this->varIndex++];
 }
 
@@ -65,44 +69,61 @@ char esapi::PushbackString::nextOctal() {
 }
 
 bool esapi::PushbackString::isHexDigit( char c ) {
+	ASSERT(c != 0);
+
 	if ( c == 0 ) return false;
 	int ch = c;
 	return (ch >= '0' && ch <= '9' ) || (ch >= 'a' && ch <= 'f' ) || (ch >= 'A' && ch <= 'F' );
 }
 
 bool esapi::PushbackString::isOctalDigit( char c ) {
+	ASSERT(c != 0);
+
 	if ( c == 0 ) return false;
 	int ch = c;
 	return ch >= '0' && ch <= '7';
 }
 
-char esapi::PushbackString::peek() {
+char esapi::PushbackString::peek() const{
 	if ( this->varPushback != 0 ) return this->varPushback;
 	if ( this->input.compare("") ) return 0;
 	if ( this->input.length() == 0 ) return 0;
 	if ( this->varIndex >= this->input.length() ) return 0;
+
+	ASSERT(varIndex < input.length());
 	return this->input[this->varIndex];
 }
 
-bool esapi::PushbackString::peek( char c ) {
+bool esapi::PushbackString::peek( char c ) const{
+	ASSERT(c != 0);
+
 	if ( this->varPushback != 0 && this->varPushback == c ) return true;
 	if ( this->input.compare("") ) return false;
 	if ( this->input.length() == 0 ) return false;
 	if ( this->varIndex >= this->input.length() ) return false;
+
+	ASSERT(varIndex < input.length());
 	return this->input[this->varIndex] == c;
 }
 
-void esapi::PushbackString::mark() {
-	this->temp = this->varPushback;
+void esapi::PushbackString::mark() const{
+	ASSERT(!input.empty());
+
+	this->varTemp = this->varPushback;
 	this->varMark = this->varIndex;
 }
 
 void esapi::PushbackString::reset()  {
-	this->varPushback = this->temp;
+	ASSERT(!input.empty());
+
+	this->varPushback = this->varTemp;
 	this->varIndex = this->varMark;
 }
 
 std::string esapi::PushbackString::remainder() {
+	ASSERT(!input.empty());
+	ASSERT(varIndex < input.length());
+
 	std::string output = this->input.substr( this->varIndex );
 	if ( this->varPushback != 0 ) {
 		output = this->varPushback + output;
