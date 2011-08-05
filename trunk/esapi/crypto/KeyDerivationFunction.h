368ae44d730dbc739061b213e6b8d887f4c900a1
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-05 23:05:48 +0000

    Removed NAMESPACE macro

--- trunk/esapi/crypto/KeyDerivationFunction.h
+++ trunk/esapi/crypto/KeyDerivationFunction.h
@@ -28,81 +28,81 @@
  * @since 2.0
  */
 
-NAMESPACE_BEGIN(esapi)
-
-typedef CryptoPP::SecByteBlock SecretKey;
-
-class KeyDerivationFunction
+namespace esapi
 {
-public:
-  /**
-   * The method is ESAPI's Key Derivation Function (KDF) that computes a
-   * derived key from the {@code keyDerivationKey} for either
-   * encryption / decryption or for authentication.
-   * 
-   * CAUTION: If this algorithm for computing derived keys from the
-   * key derivation key is ever changed, we risk breaking backward compatibility of being
-   * able to decrypt data previously encrypted with earlier / different versions
-   * of this method. Therefore, do not change this unless you are 100% certain that
-   * what you are doing will NOT change either of the derived keys for
-   * ANY "key derivation key" AT ALL!!!
-   * 
-   * NOTE: This method is generally not intended to be called separately.
-   * It is used by ESAPI's reference crypto implementation class {@code JavaEncryptor}
-   * and might be useful for someone implementing their own replacement class, but
-   * generally it is not something that is useful to application client code.
-   *
-   * @param keyDerivationKey A key used as an input to a key derivation function
-   * to derive other keys. This is the key that generally
-   * is created using some key generation mechanism such as
-   * {@link #generateSecretKey(String, int)}. The
-   * "input" key from which the other keys are derived.
-   * The derived key will have the same algorithm type * as this key. This KDK cannot be null.
-   * @param keySizeThe cipher's key size (in bits) for the {@code keyDerivationKey}.
-   * Must have a minimum size of 56 bits and be an integral multiple of 8-bits.
-   * Note: The derived key will have the same size as this.
-   * @param purposeThe purpose for the derived key. For the ESAPI reference implementation,
-   * {@code JavaEncryptor}, this must be either the string "encryption" or
-   * "authenticity", where "encryption" is used for creating a derived key to use
-   * for confidentiality, and "authenticity" is used for creating a derived key to
-   * use with a MAC to ensure message authenticity. However, since parameter serves
-   * the same purpose as the "Label" in section 5.1 of NIST SP 800-108, it really can
-   * be set to anything other than {@code null} or an empty string when called outside
-   * of {@code JavaEncryptor}.
-   * @returnThe derived {@code SecretKey} to be used according
-   * to the specified purpose.
-   * @throws NoSuchAlgorithmExceptionThe {@code keyDerivationKey} has an unsupported
-   * encryption algorithm or no current JCE provider supports
-   * "HmacSHA1".
-   * @throws EncryptionExceptionIf "UTF-8" is not supported as an encoding, then
-   * this is thrown with the original {@code UnsupportedEncodingException}
-   * as the cause. (NOTE: This should never happen as "UTF-8" is supposed to
-   * be a common encoding supported by all Java implementations. Support
-   * for it is usually in rt.jar.) * @throws InvalidKeyException Likely indicates a coding error. Should not happen.
-   * @throws EncryptionException Throw for some precondition violations.
-   */
-  static SecretKey computeDerivedKey(const SecretKey& keyDerivationKey, unsigned int keyBits, const std::string& purpose);
+  typedef CryptoPP::SecByteBlock SecretKey;
 
-  /**
-   * Check if specified algorithm name is a valid PRF that can be used.
-   * @param prfAlgNameName of the PRF algorithm; e.g., "HmacSHA1", "HmacSHA384", etc.
-   * @returnTrue if {@code prfAlgName} is supported, otherwise false.
-   */
-  //public static boolean isValidPRF(String prfAlgName);
-  //public static PRF_ALGORITHMS convertNameToPRF(String prfAlgName);
-  //public static PRF_ALGORITHMS convertIntToPRF(int selection);
+  class KeyDerivationFunction
+  {
+  public:
+    /**
+     * The method is ESAPI's Key Derivation Function (KDF) that computes a
+     * derived key from the {@code keyDerivationKey} for either
+     * encryption / decryption or for authentication.
+     * 
+     * CAUTION: If this algorithm for computing derived keys from the
+     * key derivation key is ever changed, we risk breaking backward compatibility of being
+     * able to decrypt data previously encrypted with earlier / different versions
+     * of this method. Therefore, do not change this unless you are 100% certain that
+     * what you are doing will NOT change either of the derived keys for
+     * ANY "key derivation key" AT ALL!!!
+     * 
+     * NOTE: This method is generally not intended to be called separately.
+     * It is used by ESAPI's reference crypto implementation class {@code JavaEncryptor}
+     * and might be useful for someone implementing their own replacement class, but
+     * generally it is not something that is useful to application client code.
+     *
+     * @param keyDerivationKey A key used as an input to a key derivation function
+     * to derive other keys. This is the key that generally
+     * is created using some key generation mechanism such as
+     * {@link #generateSecretKey(String, int)}. The
+     * "input" key from which the other keys are derived.
+     * The derived key will have the same algorithm type * as this key. This KDK cannot be null.
+     * @param keySizeThe cipher's key size (in bits) for the {@code keyDerivationKey}.
+     * Must have a minimum size of 56 bits and be an integral multiple of 8-bits.
+     * Note: The derived key will have the same size as this.
+     * @param purposeThe purpose for the derived key. For the ESAPI reference implementation,
+     * {@code JavaEncryptor}, this must be either the string "encryption" or
+     * "authenticity", where "encryption" is used for creating a derived key to use
+     * for confidentiality, and "authenticity" is used for creating a derived key to
+     * use with a MAC to ensure message authenticity. However, since parameter serves
+     * the same purpose as the "Label" in section 5.1 of NIST SP 800-108, it really can
+     * be set to anything other than {@code null} or an empty string when called outside
+     * of {@code JavaEncryptor}.
+     * @returnThe derived {@code SecretKey} to be used according
+     * to the specified purpose.
+     * @throws NoSuchAlgorithmExceptionThe {@code keyDerivationKey} has an unsupported
+     * encryption algorithm or no current JCE provider supports
+     * "HmacSHA1".
+     * @throws EncryptionExceptionIf "UTF-8" is not supported as an encoding, then
+     * this is thrown with the original {@code UnsupportedEncodingException}
+     * as the cause. (NOTE: This should never happen as "UTF-8" is supposed to
+     * be a common encoding supported by all Java implementations. Support
+     * for it is usually in rt.jar.) * @throws InvalidKeyException Likely indicates a coding error. Should not happen.
+     * @throws EncryptionException Throw for some precondition violations.
+     */
+    static SecretKey computeDerivedKey(const SecretKey& keyDerivationKey, unsigned int keyBits, const std::string& purpose);
 
-  /**
-   * Calculate the size of a key. The key size is given in bits, but we
-   * can only allocate them by octets (i.e., bytes), so make sure we
-   * round up to the next whole number of octets to have room for all
-   * the bits. For example, a key size of 9 bits would require 2 octets
-   * to store it. * * @param ks The key size, in bits.
-   * @return The key size, in octets, large enough to accommodate
-   * {@code ks} bits.
-   */
-  static unsigned int calcKeySize(unsigned int keyBits);
-};
+    /**
+     * Check if specified algorithm name is a valid PRF that can be used.
+     * @param prfAlgNameName of the PRF algorithm; e.g., "HmacSHA1", "HmacSHA384", etc.
+     * @returnTrue if {@code prfAlgName} is supported, otherwise false.
+     */
+    //public static boolean isValidPRF(String prfAlgName);
+    //public static PRF_ALGORITHMS convertNameToPRF(String prfAlgName);
+    //public static PRF_ALGORITHMS convertIntToPRF(int selection);
 
-NAMESPACE_END 
+    /**
+     * Calculate the size of a key. The key size is given in bits, but we
+     * can only allocate them by octets (i.e., bytes), so make sure we
+     * round up to the next whole number of octets to have room for all
+     * the bits. For example, a key size of 9 bits would require 2 octets
+     * to store it. * * @param ks The key size, in bits.
+     * @return The key size, in octets, large enough to accommodate
+     * {@code ks} bits.
+     */
+    static unsigned int calcKeySize(unsigned int keyBits);
+  };
+
+}; // NAMESPACE esapi
 
--- trunk/src/crypto/KeyDerivationFunction.cpp
+++ trunk/src/crypto/KeyDerivationFunction.cpp
@@ -36,229 +36,230 @@
  * @since 2.0
  */
 
-NAMESPACE_BEGIN(esapi)
-
-esapi::SecretKey KeyDerivationFunction::computeDerivedKey(const esapi::SecretKey& keyDerivationKey, unsigned int keySize, const std::string& purpose)
+namespace esapi
 {
-  // We would choose a larger minimum key size, but we want to be
-  // able to accept DES for legacy encryption needs.
-  ASSERT( keyDerivationKey.size()  > 0 );
-  ASSERT( keySize >= 56 ); // "Key has size of " + keySize + ", which is less than minimum of 56-bits.";
-  ASSERT( (keySize % 8) == 0 ); // "Key size (" + keySize + ") must be a even multiple of 8-bits.";
-  ASSERT( /*purpose != null &&*/ !purpose.empty()); // : "Purpose may not be null or empty.";
-  ASSERT( purpose == "authenticity" || purpose == "encryption" );
 
-  if(!(keySize >= 56))
-    {
-      std::ostringstream oss;
-      oss << "KeyDerivationFunction: key has size of " << keySize << ", which is less than minimum of 56-bits.";
-      throw std::invalid_argument(oss.str());
-    }
+  esapi::SecretKey KeyDerivationFunction::computeDerivedKey(const esapi::SecretKey& keyDerivationKey, unsigned int keySize, const std::string& purpose)
+  {
+    // We would choose a larger minimum key size, but we want to be
+    // able to accept DES for legacy encryption needs.
+    ASSERT( keyDerivationKey.size()  > 0 );
+    ASSERT( keySize >= 56 ); // "Key has size of " + keySize + ", which is less than minimum of 56-bits.";
+    ASSERT( (keySize % 8) == 0 ); // "Key size (" + keySize + ") must be a even multiple of 8-bits.";
+    ASSERT( /*purpose != null &&*/ !purpose.empty()); // : "Purpose may not be null or empty.";
+    ASSERT( purpose == "authenticity" || purpose == "encryption" );
 
-  if(!((keySize % 8) == 0))
-    {
-      std::ostringstream oss;
-      oss << "KeyDerivationFunction: key size (" << keySize << ") must be a even multiple of 8-bits.";
-      throw std::invalid_argument(oss.str());
-    }
+    if(!(keySize >= 56))
+      {
+	std::ostringstream oss;
+	oss << "KeyDerivationFunction: key has size of " << keySize << ", which is less than minimum of 56-bits.";
+	throw std::invalid_argument(oss.str());
+      }
 
-  if(purpose.empty())
-    {
-      std::ostringstream oss;
-      oss << "Purpose may not be null or empty.";
-      throw std::invalid_argument(oss.str());
-    }
+    if(!((keySize % 8) == 0))
+      {
+	std::ostringstream oss;
+	oss << "KeyDerivationFunction: key size (" << keySize << ") must be a even multiple of 8-bits.";
+	throw std::invalid_argument(oss.str());
+      }
 
-  keySize = calcKeySize( keySize );
-  ASSERT(keySize);
+    if(purpose.empty())
+      {
+	std::ostringstream oss;
+	oss << "Purpose may not be null or empty.";
+	throw std::invalid_argument(oss.str());
+      }
 
-  /*
-    byte[] derivedKey = new byte[ keySize ];
-    byte[] label;              // Same purpose as NIST SP 800-108's "label" in section 5.1.
-    byte[] context;            // See setContext() for details.
-    try {
-    label = purpose.getBytes("UTF-8");
-    context = context_.getBytes("UTF-8");
-    } catch (UnsupportedEncodingException e) {
-    throw new EncryptionException("Encryption failure (internal encoding error: UTF-8)",
-    "UTF-8 encoding is NOT supported as a standard byte encoding: " + e.getMessage(), e);
-    }
-  */
+    keySize = calcKeySize( keySize );
+    ASSERT(keySize);
 
-  // Consistency with Java implementation. This class needs to wire-up a context.
-  const std::string& label = purpose;
-  const std::string context;
+    /*
+      byte[] derivedKey = new byte[ keySize ];
+      byte[] label;              // Same purpose as NIST SP 800-108's "label" in section 5.1.
+      byte[] context;            // See setContext() for details.
+      try {
+      label = purpose.getBytes("UTF-8");
+      context = context_.getBytes("UTF-8");
+      } catch (UnsupportedEncodingException e) {
+      throw new EncryptionException("Encryption failure (internal encoding error: UTF-8)",
+      "UTF-8 encoding is NOT supported as a standard byte encoding: " + e.getMessage(), e);
+      }
+    */
 
-  // Note that keyDerivationKey is going to be some SecretKey like an AES or
-  // DESede key, but not an HmacSHA1 key. That means it is not likely
-  // going to be 20 bytes but something different. Experiments show
-  // that doesn't really matter though as the SecretKeySpec CTOR on
-  // the following line still returns the appropriate sized key for
-  // HmacSHA1. So, if keyDerivationKey was originally (say) a 56-bit
-  // DES key, then there is apparently some key-stretching going on here
-  // under the hood to create 'sk' so that it is 20 bytes. I cannot vouch
-  // for how secure this key-stretching is. Worse, it might not be specified
-  // as to *how* it is done and left to each JCE provider.
+    // Consistency with Java implementation. This class needs to wire-up a context.
+    const std::string& label = purpose;
+    const std::string context;
 
-  /*
-    SecretKey sk = new SecretKeySpec(keyDerivationKey.getEncoded(), "HmacSHA1");
-    Mac mac = null;
+    // Note that keyDerivationKey is going to be some SecretKey like an AES or
+    // DESede key, but not an HmacSHA1 key. That means it is not likely
+    // going to be 20 bytes but something different. Experiments show
+    // that doesn't really matter though as the SecretKeySpec CTOR on
+    // the following line still returns the appropriate sized key for
+    // HmacSHA1. So, if keyDerivationKey was originally (say) a 56-bit
+    // DES key, then there is apparently some key-stretching going on here
+    // under the hood to create 'sk' so that it is 20 bytes. I cannot vouch
+    // for how secure this key-stretching is. Worse, it might not be specified
+    // as to *how* it is done and left to each JCE provider.
 
-    try {
-    mac = Mac.getInstance("HmacSHA1");
-    mac.init(sk);
-    } catch( InvalidKeyException ex ) {
-    logger.error(Logger.SECURITY_FAILURE, "Created HmacSHA1 Mac but SecretKey sk has alg " + sk.getAlgorithm(), ex);
-    throw ex;
-    }
-  */
+    /*
+      SecretKey sk = new SecretKeySpec(keyDerivationKey.getEncoded(), "HmacSHA1");
+      Mac mac = null;
+
+      try {
+      mac = Mac.getInstance("HmacSHA1");
+      mac.init(sk);
+      } catch( InvalidKeyException ex ) {
+      logger.error(Logger.SECURITY_FAILURE, "Created HmacSHA1 Mac but SecretKey sk has alg " + sk.getAlgorithm(), ex);
+      throw ex;
+      }
+    */
         
-  /*
-  // Repeatedly call of HmacSHA1 hash until we've collected enough bits
-  // for the derived key. The first time through, we calculate the HmacSHA1
-  // on the "purpose" string, but subsequent calculations are performed
-  // on the previous result.
-  int ctr = 1;    // Iteration counter for NIST 800-108
-  int totalCopied = 0;
-  int destPos = 0;
-  int len = 0;
-  byte[] tmpKey = null;  // Do not declare inside do-while loop!!!
-  do {
-  //
-  // This is to make our KDF more along the line of NIST's.
-  // NIST's Special Publication 800-108 performs the following in
-  // the iterative loop of Section 5.1:
-  //       n := number of blocks required to fulfill request
-  //       for i = 1 to n, do
-  //           K(i) := PRF(KDK, [i]2 || Label || 0x00 || Context || [L]2)
-  //           result(i) := result(i-1) || K(i)
-  //       end
-  // where '||' is represents bit string concatenation, and PRF is
-  // an NIST approved pseudo-random function (such as an HMAC),
-  // KDK is the key derivation key, [i]2 is the big-endian binary
-  // representation of the iteration, and [L]2 is the bits
-  // requested by the caller, and 0x00 represents a null byte
-  // used as a separation indicator.  However, other sections of this
-  // document (Section 7.6) implies that Context is to be an
-  // optional field (based on NIST's use of the word SHOULD
-  // rather than MUST)
+    /*
+    // Repeatedly call of HmacSHA1 hash until we've collected enough bits
+    // for the derived key. The first time through, we calculate the HmacSHA1
+    // on the "purpose" string, but subsequent calculations are performed
+    // on the previous result.
+    int ctr = 1;    // Iteration counter for NIST 800-108
+    int totalCopied = 0;
+    int destPos = 0;
+    int len = 0;
+    byte[] tmpKey = null;  // Do not declare inside do-while loop!!!
+    do {
+    //
+    // This is to make our KDF more along the line of NIST's.
+    // NIST's Special Publication 800-108 performs the following in
+    // the iterative loop of Section 5.1:
+    //       n := number of blocks required to fulfill request
+    //       for i = 1 to n, do
+    //           K(i) := PRF(KDK, [i]2 || Label || 0x00 || Context || [L]2)
+    //           result(i) := result(i-1) || K(i)
+    //       end
+    // where '||' is represents bit string concatenation, and PRF is
+    // an NIST approved pseudo-random function (such as an HMAC),
+    // KDK is the key derivation key, [i]2 is the big-endian binary
+    // representation of the iteration, and [L]2 is the bits
+    // requested by the caller, and 0x00 represents a null byte
+    // used as a separation indicator.  However, other sections of this
+    // document (Section 7.6) implies that Context is to be an
+    // optional field (based on NIST's use of the word SHOULD
+    // rather than MUST)
 
-  mac.update( ByteConversionUtil.fromInt( ctr++ ) );
-  mac.update(label);
-  mac.update((byte) '\0');
-  mac.update(context);    // This is problematic for us. See Jeff Walton's
-  // analysis of ESAPI 2.0's KDF for details.
-  // Maybe for 2.1, we'll see; 2.0 too close to GA.
+    mac.update( ByteConversionUtil.fromInt( ctr++ ) );
+    mac.update(label);
+    mac.update((byte) '\0');
+    mac.update(context);    // This is problematic for us. See Jeff Walton's
+    // analysis of ESAPI 2.0's KDF for details.
+    // Maybe for 2.1, we'll see; 2.0 too close to GA.
           
-  // According to the Javadoc for Mac.doFinal(byte[]),
-  // "A call to this method resets this Mac object to the state it was
-  // in when previously initialized via a call to init(Key) or
-  // init(Key, AlgorithmParameterSpec). That is, the object is reset
-  // and available to generate another MAC from the same key, if
-  // desired, via new calls to update and doFinal." Therefore, we do
-  // not do an explicit reset().
-  tmpKey = mac.doFinal( ByteConversionUtil.fromInt( keySize ) );
+    // According to the Javadoc for Mac.doFinal(byte[]),
+    // "A call to this method resets this Mac object to the state it was
+    // in when previously initialized via a call to init(Key) or
+    // init(Key, AlgorithmParameterSpec). That is, the object is reset
+    // and available to generate another MAC from the same key, if
+    // desired, via new calls to update and doFinal." Therefore, we do
+    // not do an explicit reset().
+    tmpKey = mac.doFinal( ByteConversionUtil.fromInt( keySize ) );
           
-  if ( tmpKey.length >= keySize ) {
-  len = keySize;
-  } else {
-  len = Math.min(tmpKey.length, keySize - totalCopied);
-  }
-
-  System.arraycopy(tmpKey, 0, derivedKey, destPos, len);
-  label = tmpKey;
-  totalCopied += tmpKey.length;
-  destPos += len;
-  } while( totalCopied < keySize );
-        
-  // Don't leave remnants of the partial key in memory. (Note: we could
-  // not do this if tmpKey were declared in the do-while loop.
-  for ( int i = 0; i < tmpKey.length; i++ ) {
-  tmpKey[i] = '\0';
-  }
-  tmpKey = null;  // Make it immediately eligible for GC.
-  */
-
-  // Returned to caller
-  SecretKey derived(keySize);
-
-  // Counter
-  unsigned int ctr = 1;
-  size_t idx = 0;
-
-  while(keySize)
-    {
-      const unsigned int req = std::min((unsigned int)CryptoPP::SHA1::DIGESTSIZE, keySize);
-      const byte i[4] = { (ctr >> 24 && 0xff), (ctr >> 16 && 0xff), (ctr >> 8 && 0xff), (ctr && 0xff) };
-      const byte nil = '\0';
-
-      CryptoPP::HMAC<CryptoPP::SHA1> hmac(keyDerivationKey.BytePtr(), keyDerivationKey.SizeInBytes());
-
-      hmac.Update(i, sizeof(i));
-      hmac.Update((const byte*)label.data(), label.size());
-      hmac.Update(&nil, sizeof(nil));
-      hmac.Update((const byte*)context.data(), context.size());
-
-      // Though we continually call TruncatedFinal, we are retrieving a
-      // full block except for possibly the last block
-      hmac.TruncatedFinal(derived.BytePtr()+idx, req);
-
-      idx += req;
-      keySize -= req;        
+    if ( tmpKey.length >= keySize ) {
+    len = keySize;
+    } else {
+    len = Math.min(tmpKey.length, keySize - totalCopied);
     }
 
-  // Convert it back into a SecretKey of the appropriate type.
-  // return new SecretKeySpec(derivedKey, keyDerivationKey.getAlgorithm());
+    System.arraycopy(tmpKey, 0, derivedKey, destPos, len);
+    label = tmpKey;
+    totalCopied += tmpKey.length;
+    destPos += len;
+    } while( totalCopied < keySize );
+        
+    // Don't leave remnants of the partial key in memory. (Note: we could
+    // not do this if tmpKey were declared in the do-while loop.
+    for ( int i = 0; i < tmpKey.length; i++ ) {
+    tmpKey[i] = '\0';
+    }
+    tmpKey = null;  // Make it immediately eligible for GC.
+    */
 
-  return derived;
-}
+    // Returned to caller
+    SecretKey derived(keySize);
 
-/**
- * Check if specified algorithm name is a valid PRF that can be used.
- * @param prfAlgName  Name of the PRF algorithm; e.g., "HmacSHA1", "HmacSHA384", etc.
- * @return  True if {@code prfAlgName} is supported, otherwise false.
- */
+    // Counter
+    unsigned int ctr = 1;
+    size_t idx = 0;
 
-/*
-  public static boolean isValidPRF(String prfAlgName) {
-  for (PRF_ALGORITHMS prf : PRF_ALGORITHMS.values()) {
-  if ( prf.getAlgName().equals(prfAlgName) ) {
-  return true;
-  }
-  }
-  return false;
+    while(keySize)
+      {
+	const unsigned int req = std::min((unsigned int)CryptoPP::SHA1::DIGESTSIZE, keySize);
+	const byte i[4] = { (ctr >> 24 && 0xff), (ctr >> 16 && 0xff), (ctr >> 8 && 0xff), (ctr && 0xff) };
+	const byte nil = '\0';
+
+	CryptoPP::HMAC<CryptoPP::SHA1> hmac(keyDerivationKey.BytePtr(), keyDerivationKey.SizeInBytes());
+
+	hmac.Update(i, sizeof(i));
+	hmac.Update((const byte*)label.data(), label.size());
+	hmac.Update(&nil, sizeof(nil));
+	hmac.Update((const byte*)context.data(), context.size());
+
+	// Though we continually call TruncatedFinal, we are retrieving a
+	// full block except for possibly the last block
+	hmac.TruncatedFinal(derived.BytePtr()+idx, req);
+
+	idx += req;
+	keySize -= req;        
+      }
+
+    // Convert it back into a SecretKey of the appropriate type.
+    // return new SecretKeySpec(derivedKey, keyDerivationKey.getAlgorithm());
+
+    return derived;
   }
 
-  public static PRF_ALGORITHMS convertNameToPRF(String prfAlgName) {
-  for (PRF_ALGORITHMS prf : PRF_ALGORITHMS.values()) {
-  if ( prf.getAlgName().equals(prfAlgName) ) {
-  return prf;
-  }
-  }
-  throw new IllegalArgumentException("Algorithm name " + prfAlgName +
-  " not a valid PRF algorithm name for the ESAPI KDF.");
-  }
+  /**
+   * Check if specified algorithm name is a valid PRF that can be used.
+   * @param prfAlgName  Name of the PRF algorithm; e.g., "HmacSHA1", "HmacSHA384", etc.
+   * @return  True if {@code prfAlgName} is supported, otherwise false.
+   */
+
+  /*
+    public static boolean isValidPRF(String prfAlgName) {
+    for (PRF_ALGORITHMS prf : PRF_ALGORITHMS.values()) {
+    if ( prf.getAlgName().equals(prfAlgName) ) {
+    return true;
+    }
+    }
+    return false;
+    }
+
+    public static PRF_ALGORITHMS convertNameToPRF(String prfAlgName) {
+    for (PRF_ALGORITHMS prf : PRF_ALGORITHMS.values()) {
+    if ( prf.getAlgName().equals(prfAlgName) ) {
+    return prf;
+    }
+    }
+    throw new IllegalArgumentException("Algorithm name " + prfAlgName +
+    " not a valid PRF algorithm name for the ESAPI KDF.");
+    }
     
-  public static PRF_ALGORITHMS convertIntToPRF(int selection) {
-  for (PRF_ALGORITHMS prf : PRF_ALGORITHMS.values()) {
-  if ( prf.getValue() == selection ) {
-  return prf;
-  }
-  }
-  throw new IllegalArgumentException("No KDF PRF algorithm found for value name " + selection);    
-  }
-*/
+    public static PRF_ALGORITHMS convertIntToPRF(int selection) {
+    for (PRF_ALGORITHMS prf : PRF_ALGORITHMS.values()) {
+    if ( prf.getValue() == selection ) {
+    return prf;
+    }
+    }
+    throw new IllegalArgumentException("No KDF PRF algorithm found for value name " + selection);    
+    }
+  */
 
-/**
- * Calculate the size of a key.
- */
-unsigned int KeyDerivationFunction::calcKeySize(unsigned int keyBits)
-{
-  ASSERT(keyBits >= 56);
-  ASSERT(0 == keyBits % 8);
+  /**
+   * Calculate the size of a key.
+   */
+  unsigned int KeyDerivationFunction::calcKeySize(unsigned int keyBits)
+  {
+    ASSERT(keyBits >= 56);
+    ASSERT(0 == keyBits % 8);
 
-  SafeInt<unsigned int> k(keyBits);
-  return (unsigned int)((k + 7) / 8);
-}
+    SafeInt<unsigned int> k(keyBits);
+    return (unsigned int)((k + 7) / 8);
+  }
 
-NAMESPACE_END
+}; // NAMESPACE esapi
