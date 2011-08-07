e74bd66be239ee5562d11a155c9a5f57ab119d56
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-07 03:38:27 +0000

    Added light testing on AES/CBC

--- trunk/test/TestMain.cpp
+++ trunk/test/TestMain.cpp
@@ -1,10 +1,12 @@
 void VerifySecretKey();
+void VerifyKeyGenerator();
 void VerifyCryptoHelper();
 void VerifyKeyDerivationFunction();
 
 int main(int, char**)
 {
 	VerifySecretKey();
+	VerifyKeyGenerator();
 	VerifyCryptoHelper();
     VerifyKeyDerivationFunction();
 
--- trunk/test/crypto/KeyGeneratorTest.cpp
+++ trunk/test/crypto/KeyGeneratorTest.cpp
@@ -9,6 +9,20 @@ using namespace boost::unit_test;
 #include <string>
 using std::string;
 
+#include <memory>
+using std::auto_ptr;
+
+#include <crypto/SecretKey.h>
 #include <crypto/KeyGenerator.h>
+using esapi::SecretKey;
 using esapi::KeyGenerator;
+
+void VerifyKeyGenerator()
+{
+	auto_ptr<KeyGenerator> t1(KeyGenerator::getInstance("AES/CBC/PKCS5"));
+	t1->init(128);
+    cout << t1->algorithm() << endl;
+
+	SecretKey k1 = t1->generateKey();
+}
 
