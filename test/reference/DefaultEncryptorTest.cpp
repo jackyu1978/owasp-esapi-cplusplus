b9c190e4c4efd584539e7b0b4a764b21cb637d01
noloader <noloader@gmail.com>
2011-08-12 21:47:42 +0000

    Added empty DefaultEncryptor test

---
+++ test/reference/DefaultEncryptorTest.cpp
@@ -0,0 +1,35 @@
+/*
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ *
+ * @author Kevin Wall, kevin.w.wall@gmail.com
+ * @author Jeffrey Walton, noloader@gmail.com
+ * @author David Anderson, david.anderson@aspectsecurity.com
+ */
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+#if !defined(ESAPI_OS_WINDOWS)
+# define BOOST_TEST_DYN_LINK
+# include <boost/test/unit_test.hpp>
+  using namespace boost::unit_test;
+#endif
+
+#include <string>
+using std::string;
+
+#include <reference/DefaultEncryptor.h>
+using esapi::DefaultEncryptor;
+
+BOOST_AUTO_TEST_CASE( VerifyDefaultEncryptor )
+{
+	BOOST_MESSAGE( "Verifying DefaultEncryptor class" );
+}
