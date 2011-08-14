4b81ab389f67eb932bea9621a3031a5470461e70
noloader <noloader@gmail.com>
2011-08-14 04:26:21 +0000

    Added InvalidArgumentException.

---
+++ esapi/errors/InvalidArgumentException.h
@@ -0,0 +1,36 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ */
+
+#pragma once
+
+#include "EsapiCommon.h"
+#include "errors/EnterpriseSecurityException.h"
+
+#include <stdexcept>
+#include <string>
+
+// TODO: Finish Porting from Java
+
+namespace esapi {
+
+class ESAPI_EXPORT InvalidArgumentException : public EnterpriseSecurityException
+{
+public:
+	explicit InvalidArgumentException(const std::string &message)
+    : EnterpriseSecurityException(message, message)
+  {
+  }
+  	explicit InvalidArgumentException(const std::string &userMessage, const std::string &logMessage)
+    : EnterpriseSecurityException(userMessage, logMessage)
+  {
+  }
+};
+
+} // NAMESPACE
