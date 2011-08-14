89c77ea26277e2b504cf33cf0e2db4e9a8f498fb
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-14 12:29:40 +0000

    Whitespace

--- trunk/src/crypto/MessageDigest.cpp
+++ trunk/src/crypto/MessageDigest.cpp
@@ -1,12 +1,12 @@
 /**
-* OWASP Enterprise Security API (ESAPI)
-*
-* This file is part of the Open Web Application Security Project (OWASP)
-* Enterprise Security API (ESAPI) project. For details, please see
-* http://www.owasp.org/index.php/ESAPI.
-*
-* Copyright (c) 2011 - The OWASP Foundation
-*/
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ */
 
 #include "EsapiCommon.h"
 #include "crypto/MessageDigest.h"
@@ -87,7 +87,7 @@ namespace esapi
     // MessageDigest md = MessageDigest.getInstance("Foo");
     // md.update(scratch);
 
-	// We only have InvalidArgumentException and EncryptionException
+    // We only have InvalidArgumentException and EncryptionException
     std::ostringstream oss;
     oss << "Algorithm \'" << algorithm << "\' is not supported.";
     throw InvalidArgumentException(oss.str());
@@ -117,8 +117,8 @@ namespace esapi
   }
 
   /**
-  * Updates the digest using the specified byte.
-  */
+   * Updates the digest using the specified byte.
+   */
   template <class HASH>
   void MessageDigestImpl<HASH>::update(byte input)
   {
@@ -126,8 +126,8 @@ namespace esapi
   }
 
   /**
-  * Updates the digest using the specified array of bytes. 
-  */
+   * Updates the digest using the specified array of bytes. 
+   */
   template <class HASH>
   void MessageDigestImpl<HASH>::update(const byte input[], size_t size)
   {
@@ -138,8 +138,8 @@ namespace esapi
   }
 
   /**
-  * Updates the digest using the specified array of bytes.
-  */
+   * Updates the digest using the specified array of bytes.
+   */
   template <class HASH>
   void MessageDigestImpl<HASH>::update(const std::vector<byte>& input)
   {
@@ -149,8 +149,8 @@ namespace esapi
   }
 
   /**
-  * Updates the digest using the specified array of bytes, starting at the specified offset.
-  */
+   * Updates the digest using the specified array of bytes, starting at the specified offset.
+   */
   template <class HASH>
   void MessageDigestImpl<HASH>::update(const byte input[], size_t size, size_t offset, size_t len)
     throw(InvalidArgumentException, EncryptionException)
@@ -158,12 +158,12 @@ namespace esapi
     //ASSERT(input);
     //ASSERT(size);
 
-	// This Java program will throw a NullPointerException
+    // This Java program will throw a NullPointerException
     // byte[] scratch = null;
     // MessageDigest md = MessageDigest.getInstance("MD5");
     // md.update(scratch);
 
-	// This Java program is OK
+    // This Java program is OK
     // byte[] scratch = new byte[0];
     // MessageDigest md = MessageDigest.getInstance("MD5");
     // md.update(scratch);
@@ -172,47 +172,47 @@ namespace esapi
     if(!input)
       throw InvalidArgumentException("The input array or size is not valid");
 
-	// This Java program will throw an IllegalArgumentException
+    // This Java program will throw an IllegalArgumentException
     // byte[] scratch = new byte[16];
     // MessageDigest md = MessageDigest.getInstance("MD5");
     // md.update(scratch, 1, 16);
 
     try
-    {
-      // Pointer wrap?
-      SafeInt<size_t> safe1((size_t)input);
-      safe1 += size;
+      {
+        // Pointer wrap?
+        SafeInt<size_t> safe1((size_t)input);
+        safe1 += size;
 
-      // Within bounds?
-      SafeInt<size_t> safe2(offset);
-      safe2 += len;
-      if((size_t)safe2 > size)
-        throw InvalidArgumentException("The buffer is too small for specified offset and length");
+        // Within bounds?
+        SafeInt<size_t> safe2(offset);
+        safe2 += len;
+        if((size_t)safe2 > size)
+          throw InvalidArgumentException("The buffer is too small for the specified offset and length");
 
-      m_hash.Update(input+offset, len);
-    }
+        m_hash.Update(input+offset, len);
+      }
     catch(SafeIntException&)
-    {
-      throw EncryptionException("Integer overflow detected");
-    }
+      {
+        throw EncryptionException("Integer overflow detected");
+      }
     catch(CryptoPP::Exception& ex)
-    {
-      throw EncryptionException(std::string("Internal error: ") + ex.what());
-    }
+      {
+        throw EncryptionException(std::string("Internal error: ") + ex.what());
+      }
   }
 
   /**
-  * Performs a final update on the digest using the specified array of bytes, then completes the
-  * digest computation.
-  */
+   * Performs a final update on the digest using the specified array of bytes, then completes the
+   * digest computation.
+   */
   // template <class HASH>
   // byte[] MessageDigestImpl<HASH>::digest(byte input[], size_t size)
   // {
   // }
 
   /**
-  * Completes the hash computation by performing final operations such as padding.
-  */
+   * Completes the hash computation by performing final operations such as padding.
+   */
   template <class HASH>
   unsigned int MessageDigestImpl<HASH>::digest(byte buf[], size_t size, size_t offset, size_t len)
     throw(InvalidArgumentException, EncryptionException)
@@ -220,14 +220,14 @@ namespace esapi
     // ASSERT(buf);
     // ASSERT(size);
 
-	// This Java program will throw an IllegalArgumentException
+    // This Java program will throw an IllegalArgumentException
     // MessageDigest md = MessageDigest.getInstance("MD5");
     // int size = md.digest(null, 0, 0);
 
     if(!buf || !size)
       throw InvalidArgumentException("The buffer array or size is not valid");
 
-	// This Java program will throw an DigestException
+    // This Java program will throw an DigestException
     // byte[] scratch = new byte[1];
     // MessageDigest md = MessageDigest.getInstance("MD5");
     // int size = md.digest(scratch, 0, 0);
@@ -238,44 +238,44 @@ namespace esapi
     // int ret = md.digest(scratch, 0, 15);
 
     if(size < (unsigned int)m_hash.DigestSize() || len < (unsigned int)m_hash.DigestSize())
-    {
-      std::ostringstream oss;
-      oss << "Length must be at least " << m_hash.DigestSize() << " for " << getAlgorithm();
-      throw InvalidArgumentException(oss.str());
-    }
+      {
+        std::ostringstream oss;
+        oss << "Length must be at least " << m_hash.DigestSize() << " for " << getAlgorithm();
+        throw InvalidArgumentException(oss.str());
+      }
 
     const size_t req = std::min(size, std::min((size_t)HASH::DIGESTSIZE, len));
 
     try
-    {
-      // Pointer wrap?
-      SafeInt<size_t> safe1((size_t)buf);
-      safe1 += size;
+      {
+        // Pointer wrap?
+        SafeInt<size_t> safe1((size_t)buf);
+        safe1 += size;
 
-      // Within bounds?
-      SafeInt<size_t> safe2(offset);
-      safe2 += len;
-      if((size_t)safe2 > size)
-        throw InvalidArgumentException("The buffer is too small for specified offset and length");
+        // Within bounds?
+        SafeInt<size_t> safe2(offset);
+        safe2 += len;
+        if((size_t)safe2 > size)
+          throw InvalidArgumentException("The buffer is too small for the specified offset and length");
 
-      // TruncatedFinal returns the requested number of bytes and restarts the hash.
-      m_hash.TruncatedFinal(buf+offset, req);
-    }
+        // TruncatedFinal returns the requested number of bytes and restarts the hash.
+        m_hash.TruncatedFinal(buf+offset, req);
+      }
     catch(SafeIntException&)
-    {
-      throw EncryptionException("Integer overflow detected");
-    }
+      {
+        throw EncryptionException("Integer overflow detected");
+      }
     catch(CryptoPP::Exception& ex)
-    {
-      throw EncryptionException(std::string("Internal error: ") + ex.what());
-    }
+      {
+        throw EncryptionException(std::string("Internal error: ") + ex.what());
+      }
 
     return req;
   }
 
   /**
-  * Completes the hash computation by performing final operations such as padding.
-  */
+   * Completes the hash computation by performing final operations such as padding.
+   */
   template <class HASH>
   unsigned int  MessageDigestImpl<HASH>::digest(std::vector<byte>& buf, size_t offset, size_t len)
   {
