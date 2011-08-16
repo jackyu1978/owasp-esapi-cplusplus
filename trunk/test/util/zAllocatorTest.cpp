1c2ae11e27f64b16bf06a7dbc1f115ff38e50373
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-16 19:08:04 +0000

    Added zAllocator self tests, including iterations/comparisons against a container with std allocator Moved SecureIntArray into EsapiCommon.h Renamed zAllocator.hpp -> zAllocator.h

--- trunk/test/util/zAllocatorTest.cpp
+++ trunk/test/util/zAllocatorTest.cpp
@@ -12,25 +12,661 @@
  * @author David Anderson, david.anderson@aspectsecurity.com
  */
 
-#include <iostream>
-using std::cout;
-using std::cerr;
-using std::endl;
-
 #if !defined(ESAPI_OS_WINDOWS)
 # define BOOST_TEST_DYN_LINK
 # include <boost/test/unit_test.hpp>
 using namespace boost::unit_test;
 #endif
 
-#include <string>
-using std::string;
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
 
-#include <util/zAllocator.hpp>
+#include <vector>
+using std::vector;
+
+#include <list>
+using std::list;
+
+#include <algorithm>
+using std::equal;
+
+#include <sstream>
+using std::ostringstream;
+
+#include <EsapiCommon.h>
+#include <util/zAllocator.h>
 using esapi::zallocator;
+using esapi::SecureByteArray;
+using esapi::SecureIntArray;
+
+void VerifyAllocationWrap();
+void VerifyListAllocation();
+
+void VerifySecureByteArray();
+void VerifySecureIntArray();
+
+struct Useless
+{
+  byte unused[4096];
+};
 
 BOOST_AUTO_TEST_CASE( VerifyAllocator )
 {
   BOOST_MESSAGE( "Verifying zAllocator class" );
+
+  VerifyAllocationWrap();
+  VerifyListAllocation();
+  VerifySecureByteArray();
+  VerifySecureIntArray();
+}
+
+void VerifyAllocationWrap()
+{
+  bool success = false;
+
+  // Negative
+  try
+    {
+      success = false;
+      const size_t excess = (std::numeric_limits<size_t>::max() / sizeof(Useless)) << 2;
+      std::vector< Useless, zallocator<Useless> > vv (excess);
+    }
+  catch(std::exception&)
+    {
+      success = true;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to catch wrap");
+        
+}
+
+void VerifyListAllocation()
+{
+  bool success = false;
+  try
+    {
+      success = false;
+      std::list< int, zallocator<int> > ll;
+
+      success = (ll.size() == 0);
+    }
+  catch(std::exception&)
+    {
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Unexpected list");
+
+  try
+    {
+      success = false;
+      std::list< int, zallocator<int> > ll;
+      ll.push_front(1);
+
+      success = (ll.size() == 1);
+    }
+  catch(std::exception&)
+    {
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to insert into list");
+
+  try
+    {
+      success = true;
+      std::list< int, zallocator<int> > ll;
+      ll.push_front(1);
+      ll.push_front(2);
+
+      std::list< int, zallocator<int> >::const_iterator it = ll.begin();
+      success |= (*it == 1);
+      it++;
+      success |= (*it == 2);
+    }
+  catch(std::exception&)
+    {
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to iterate list");
+
+  try
+    {
+      success = false;
+      std::list< int, zallocator<int> > ll;
+      ll.push_front(1);
+      ll.push_front(2);
+
+      std::list< int > kk;
+      kk.push_front(1);
+      kk.push_front(2);
+
+      success = equal(ll.begin(), ll.end(), kk.begin());
+    }
+  catch(std::exception&)
+    {
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to compare lists");
+
+  try
+    {
+      success = false;
+      std::list< int, zallocator<int> > ll;
+      ll.push_front(1);
+      ll.push_front(2);
+
+      std::list< int > kk;
+      kk.push_front(2);
+      kk.push_front(1);
+
+      success = !equal(ll.begin(), ll.end(), kk.begin());
+    }
+  catch(std::exception&)
+    {
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to compare lists");
+
+  try
+    {
+      success = false;
+      std::list< int, zallocator<int> > ll;
+      ll.push_front(1);
+      ll.push_front(2);
+      ll.push_front(3);
+      ll.push_front(4);
+
+      std::list< int > kk;
+      kk.push_front(1);
+      kk.push_front(2);
+
+      success = !equal(ll.begin(), ll.end(), kk.begin());
+    }
+  catch(std::exception&)
+    {
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to compare lists");
+
+      success = true;
+      std::list< int, zallocator<int> > ll;
+      ll.push_front(1);
+      ll.push_front(2);
+
+      std::list< int, zallocator<int> >::iterator it = ll.begin();
+      success |= (*it == 1);
+
+      ll.erase(it);
+      it = ll.begin();
+      
+      success |= (*it == 2);
+}
+
+void VerifySecureByteArray()
+{
+  bool success = false;
+
+  // Negative (can't run this test)
+  // memory access violation at address: 0x00000000: no mapping at fault address
+  // try
+  //  {
+  //    success = false;
+  //    SecureByteArray vv;
+  //    byte p = vv[0];
+  //  }
+  //catch(...)
+  //  {
+  //    success = true;
+  //  }
+  // BOOST_CHECK_MESSAGE(success, "Failed to catch empty operator[]");
+
+  // Negative
+  try
+    {
+      success = false;
+      SecureByteArray vv;
+      byte p = vv.at(0);
+    }
+  catch(std::exception&)
+    {
+      success = true;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to catch empty at()");
+
+  // Negative (can't run this test)
+  // memory access violation at address: 0x00000000: no mapping at fault address
+  // try
+  //  {
+  //    success = false;
+  //    SecureByteArray vv;
+  //    byte* pb = &vv[0];
+  //  }
+  //catch(std::exception&)
+  //  {
+  //    success = true;
+  //  }
+  // BOOST_CHECK_MESSAGE(success, "Failed to catch address of empty array");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureByteArray vv(1);
+
+      success = (0 == vv.at(0));
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureByteArray vv(1);
+
+      success = (0 == vv[0]);
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureByteArray vv(1);
+
+      byte* pb = &vv[0];
+
+      success = (0 == *pb);
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureByteArray vv;
+      vv.push_back(1);
+
+      success = (1 == vv[0]);
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureByteArray vv;
+      vv.push_back(1);
+
+      success = (1 == vv.at(0));
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureByteArray vv;
+      vv.push_back(1);
+      vv.push_back(2);
+      vv.push_back(3);
+
+      success = (3 == vv.size());
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed size() test");
+
+  // Positive
+  try
+    {
+      success = false;
+
+      SecureByteArray vv;
+      vv.push_back(1);
+      vv.push_back(2);
+      vv.push_back(3);
+
+      std::vector< byte > uu;
+      uu.push_back(1);
+      uu.push_back(2);
+      uu.push_back(3);
+
+      success = equal(vv.begin(), vv.end(), uu.begin());
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed equal array test");
+
+  // Positive
+  try
+    {
+      success = false;
+
+      SecureByteArray vv;
+      vv.push_back(1);
+      vv.push_back(2);
+      vv.push_back(3);
+      vv.push_back(4);
+
+      std::vector< byte > uu;
+      uu.push_back(1);
+      uu.push_back(2);
+
+      success = !equal(vv.begin(), vv.end(), uu.begin());
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed equal array test");
+
+}
+
+void VerifySecureIntArray()
+{
+  bool success = false;
+
+  // Negative (can't run this test)
+  // memory access violation at address: 0x00000000: no mapping at fault address
+  // try
+  //  {
+  //    success = false;
+  //    SecureIntArray vv;
+  //    byte p = vv[0];
+  //  }
+  //catch(...)
+  //  {
+  //    success = true;
+  //  }
+  //catch(...)
+  //  {
+  //    cerr << "Caught unknown exception" << endl;
+  //  }
+  // BOOST_CHECK_MESSAGE(success, "Failed to catch empty operator[]");
+
+  // Negative
+  try
+    {
+      success = false;
+      SecureIntArray vv;
+      byte p = vv.at(0);
+    }
+  catch(std::exception&)
+    {
+      success = true;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed to catch empty at()");
+
+  // Negative (can't run this test)
+  // memory access violation at address: 0x00000000: no mapping at fault address
+  // try
+  //  {
+  //    success = false;
+  //    SecureIntArray vv;
+  //    byte* pb = &vv[0];
+  //  }
+  //catch(std::exception&)
+  //  {
+  //    success = true;
+  //  }
+  //catch(...)
+  //  {
+  //    cerr << "Caught unknown exception" << endl;
+  //  }
+  // BOOST_CHECK_MESSAGE(success, "Failed to catch address of empty array");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureIntArray vv(1);
+
+      success = (0 == vv.at(0));
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureIntArray vv(1);
+
+      success = (0 == vv[0]);
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureIntArray vv(1);
+      int* pi = &vv[0];
+
+      success = (0 == *pi);
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureIntArray vv;
+      vv.push_back(1);
+
+      success = (1 == vv[0]);
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureIntArray vv;
+      vv.push_back(1);
+
+      success = (1 == vv.at(0));
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed single element test");
+
+  // Positive
+  try
+    {
+      success = false;
+      SecureIntArray vv;
+      vv.push_back(1);
+      vv.push_back(2);
+      vv.push_back(3);
+
+      success = (3 == vv.size());
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed size() test");
+
+  // Positive
+  try
+    {
+      success = false;
+
+      SecureIntArray vv;
+      vv.push_back(1);
+      vv.push_back(2);
+      vv.push_back(3);
+
+      std::vector< byte > uu;
+      uu.push_back(1);
+      uu.push_back(2);
+      uu.push_back(3);
+
+      success = equal(vv.begin(), vv.end(), uu.begin());
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed equal array test");
+
+  // Positive
+  try
+    {
+      success = false;
+
+      SecureIntArray vv;
+      vv.push_back(1);
+      vv.push_back(2);
+      vv.push_back(3);
+      vv.push_back(4);
+
+      std::vector< byte > uu;
+      uu.push_back(1);
+      uu.push_back(2);
+
+      success = !equal(vv.begin(), vv.end(), uu.begin());
+    }
+  catch(std::exception& ex)
+    {
+      cerr << "Caught " << ex.what() << endl;
+    }
+  catch(...)
+    {
+      cerr << "Caught unknown exception" << endl;
+    }
+  BOOST_CHECK_MESSAGE(success, "Failed equal array test");
 }
 
