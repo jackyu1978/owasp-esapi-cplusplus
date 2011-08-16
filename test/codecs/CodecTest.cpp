2d76fc56bc063dbac51a8555ce4181dcd4709b61
deige101 <deige101@gmail.com>
2011-08-16 14:05:39 +0000

    expanded test cases

--- test/codecs/CodecTest.cpp
+++ test/codecs/CodecTest.cpp
@@ -30,6 +30,18 @@ BOOST_AUTO_TEST_CASE( CodecContainsCharacter)
   Codec codec;
 
   BOOST_CHECK(codec.containsCharacter('a',"asdf"));
+  BOOST_CHECK(codec.containsCharacter('f',"asdf"));
+  BOOST_CHECK(codec.containsCharacter('b',"asdf") == false);
+  BOOST_CHECK(codec.containsCharacter(' ',"asdf") == false);
+  BOOST_CHECK(codec.containsCharacter('\0',"asdf") == false);
+  BOOST_CHECK(codec.containsCharacter(0,"") == false);
+
+  BOOST_CHECK(codec.containsCharacter('a',(char*)"asdf",4));
+  BOOST_CHECK(codec.containsCharacter('f',(char*)"asdf",4));
+  BOOST_CHECK(codec.containsCharacter('b',(char*)"asdf",4) == false);
+  BOOST_CHECK(codec.containsCharacter(' ',(char*)"asdf",4) == false);
+  BOOST_CHECK(codec.containsCharacter('\0',(char*)"asdf",4) == false);
+  BOOST_CHECK(codec.containsCharacter(0,0,0) == false);
 }
 
 BOOST_AUTO_TEST_CASE( CodecEncode )
@@ -37,4 +49,35 @@ BOOST_AUTO_TEST_CASE( CodecEncode )
   Codec codec;
 
   BOOST_CHECK(codec.encode("a",4,"asdf").compare("asdf")==0);
+  BOOST_CHECK(codec.encode("",4,"asdf").compare("asdf")==0);
+}
+
+BOOST_AUTO_TEST_CASE( CodecToHex )
+{
+	Codec codec;
+
+	BOOST_CHECK(codec.toHex('a')[0] == 0x61);
+	BOOST_CHECK(codec.toHex('b')[0] == 0x62);
+	BOOST_CHECK(codec.toHex('a')[0] != 0x62);
+}
+
+BOOST_AUTO_TEST_CASE( CodecToOctal )
+{
+	Codec codec;
+
+	BOOST_CHECK(codec.toOctal('a')[0] == 0141);
+	BOOST_CHECK(codec.toOctal('b')[0] == 0142);
+	BOOST_CHECK(codec.toOctal('a')[0] != 0142);
+}
+
+BOOST_AUTO_TEST_CASE( CodecGetHexForNonAlphanumeric )
+{
+	Codec codec;
+
+	//BOOST_CHECK(codec.getHexForNonAlphanumeric('!')[0] == 0x21);
+	BOOST_CHECK_MESSAGE(atoi(codec.getHexForNonAlphanumeric('!').c_str()) == 21, "getHexForNonAlphanumeric('!') == " << codec.getHexForNonAlphanumeric('!'));
+	BOOST_CHECK(atoi(codec.getHexForNonAlphanumeric('"').c_str()) == 22);
+	BOOST_CHECK(codec.getHexForNonAlphanumeric('!')[0] != 22);
+
+	BOOST_CHECK(atoi(codec.getHexForNonAlphanumeric('a').c_str()) == 0);
 }
