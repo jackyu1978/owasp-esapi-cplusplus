3a1ed5243396eec1539eeb48f1a8562eae55d590
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-18 13:45:05 +0000

    Added conditional define to zAllocator per Johnathan Wakely's recommendations

--- trunk/esapi/util/zAllocator.h
+++ trunk/esapi/util/zAllocator.h
@@ -28,78 +28,92 @@
 namespace esapi
 {
   template<typename T>
-  class zallocator
-  {
-  public:
-
-    typedef T value_type;
-    typedef value_type* pointer;
-    typedef const value_type* const_pointer;
-    typedef value_type& reference;
-    typedef const value_type& const_reference;
-    typedef std::size_t size_type;
-    typedef std::ptrdiff_t difference_type;
-
-  public:
-
-    // tame the optimizer
-    static volatile void* g_dummy;
-
-    template<typename U>
-    struct rebind {
-      typedef zallocator<U> other;
-    };
-
-    inline explicit zallocator() { }
-    inline ~zallocator() { }
-    inline zallocator(zallocator const&) { }
-
-    template<typename U>
-    inline explicit zallocator(zallocator<U> const&) { }
-
-    // address
-    inline pointer address(reference r) { return &r; }
-    inline const_pointer address(const_reference r) { return &r; }
-
-    // memory allocation
-    inline pointer allocate(size_type cnt, typename std::allocator<void>::const_pointer = 0)
+    class zallocator
     {
-      // Overflow/wrap is checked for by the container. Set a breakpoint
-      // on max_size() and view the call stack (ie, the preceeding frame)
-      // for verification.
-      return reinterpret_cast<pointer>(::operator new(cnt * sizeof (T))); 
-    }
+    public:
+
+      typedef T value_type;
+      typedef value_type* pointer;
+      typedef const value_type* const_pointer;
+      typedef value_type& reference;
+      typedef const value_type& const_reference;
+      typedef std::size_t size_type;
+      typedef std::ptrdiff_t difference_type;
+
+    public:
+
+      // tame the optimizer
+      static volatile void* g_dummy;
+
+      template<typename U>
+	    struct rebind {
+	    typedef zallocator<U> other;
+      };
+
+      inline explicit zallocator() { }
+      inline ~zallocator() { }
+      inline zallocator(zallocator const&) { }
+
+      template<typename U>
+      inline explicit zallocator(zallocator<U> const&) { }
+
+      // address
+      inline pointer address(reference r) { return &r; }
+      inline const_pointer address(const_reference r) { return &r; }
+
+      // memory allocation
+      inline pointer allocate(size_type cnt, typename std::allocator<void>::const_pointer = 0)
+      {
+        // Overflow/wrap is checked for by the container. Set a breakpoint
+        // on max_size() and view the call stack (ie, the preceeding frame)
+        // for verification.
+        return reinterpret_cast<pointer>(::operator new(cnt * sizeof (T))); 
+      }
     
-    inline void deallocate(pointer p, size_type n)
-    {
-      // Pointer 'p' is checked for validity by the container. Because 'p'
-      // is assigned to a static volatile pointer, the optimizer currently
-      // does not optimize out the ::memset as dead code. Set a breakpoint
-      // on line 66 and view the call stack (ie, the preceeding frame) for
-      // verification. 
-      ::memset(p, 0x00, n);
-      g_dummy = p;
-      ::operator delete(p);
-    }
+      inline void deallocate(pointer p, size_type n)
+      {
+        // Pointer 'p' is checked for validity by the container. Because 'p'
+        // is assigned to a static volatile pointer, the optimizer currently
+        // does not optimize out the ::memset as dead code. Set a breakpoint
+        // on line 66 and view the call stack (ie, the preceeding frame) for
+        // verification. 
+        ::memset(p, 0x00, n);
+        g_dummy = p;
+        ::operator delete(p);
+      }
 
-    // size
-    inline size_type max_size() const
-    {
-      return std::numeric_limits<size_type>::max() / sizeof(T);
-    }
+      // size
+      inline size_type max_size() const
+      {
+        return std::numeric_limits<size_type>::max() / sizeof(T);
+      }
 
-    // construction/destruction
-    // inline void construct(pointer p, const T& t) { new(p) T(t); }
-    // Added for Fedora 15/GCC 4.6. Was the previous construct non-compliant?
-    inline void construct(pointer p, const T& t = T()) { new(p) T(t); }
-    inline void destroy(pointer p) { p->~T(); }
+      // construction/destruction
+      inline void construct(pointer p, const T& t) { new(p) T(t); }
+      inline void destroy(pointer p) { p->~T(); }
 
-    inline bool operator==(zallocator const&) const { return true; }
-    inline bool operator!=(zallocator const& a) const { return !operator==(a); }
-  };
+      inline bool operator==(zallocator const&) const { return true; }
+      inline bool operator!=(zallocator const& a) const { return !operator==(a); }
+
+      // http://code.google.com/p/owasp-esapi-cplusplus/issues/detail?id=11
+#if defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC__ == 4 && __GNUC_MINOR__ < 7
+      template<typename U, typename... Args>
+        void construct(U* p, Args&&... a)
+      {
+        ::new ((void*)p) U(std::forward<Args>(a)...);
+      }
+
+      template<typename U>
+        void destroy(U* p)
+        {
+          delete p;
+        }
+#endif
+
+    };
 
   // Storage and intialization
   template <class T>
-  volatile void* zallocator<T>::g_dummy = NULL;
+    volatile void* zallocator<T>::g_dummy = NULL;
 
 } // esapi
