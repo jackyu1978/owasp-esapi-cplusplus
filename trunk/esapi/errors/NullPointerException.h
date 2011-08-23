251c6b71c59f4d138b48f14b3f557a358e562938
deige101@gmail.com <deige101@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-23 18:58:30 +0000

    Layed out HashTrie.h and made Trie functions virtual. Several methods still need to be implemented.

---
+++ trunk/esapi/errors/NullPointerException.h
@@ -0,0 +1,32 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ */
+
+#pragma once
+
+#include "EsapiCommon.h"
+#include "errors/EnterpriseSecurityException.h"
+
+#include <stdexcept>
+#include <string>
+
+// TODO: Finish Porting from Java
+
+namespace esapi {
+
+class ESAPI_EXPORT NullPointerException : public EnterpriseSecurityException
+{
+public:
+	explicit NullPointerException(const std::string &message)
+    : EnterpriseSecurityException(message, message)
+  {
+  }
+};
+
+} // NAMESPACE
