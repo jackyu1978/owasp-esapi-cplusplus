81d5cd7d1730c19ab85970d715861af83520e4ca
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-08-27 02:43:50 +0000

    Cleared warnings on GCC 4.6

--- trunk/src/codecs/WindowsCodec.cpp
+++ trunk/src/codecs/WindowsCodec.cpp
@@ -18,15 +18,15 @@ std::string esapi::WindowsCodec::encodeCharacter( const char immune[], size_t le
 	ASSERT (c != 0);
 
 	// check for immune characters
-	for (unsigned int i=0; i<length; i++) {
+	for (size_t i=0; i<length; i++) {
 		if (immune[i] == c)
 			return (std::string)""+c;
 	}
 
 	// check for alphanumeric characters
 	std::string hex = esapi::Codec::getHexForNonAlphanumeric( c );
-	if ( hex.compare("") == 0 ) {
-		return (std::string)""+c;
+	if ( hex.compare("") == '\0' ) {
+		return std::string(1, c);
 	}
 
 	return (std::string)"^" + c;
@@ -35,15 +35,15 @@ std::string esapi::WindowsCodec::encodeCharacter( const char immune[], size_t le
 char esapi::WindowsCodec::decodeCharacter( PushbackString& input) const {
 	input.mark();
 	char first = input.next();
-	if ( first == 0 ) {
+	if ( first == '\0' ) {
 		input.reset();
-		return NULL;
+		return '\0';
 	}
 
 	// if this is not an encoded character, return null
 	if ( first != '^' ) {
 		input.reset();
-		return NULL;
+		return '\0';
 	}
 
 	char second = input.next();
