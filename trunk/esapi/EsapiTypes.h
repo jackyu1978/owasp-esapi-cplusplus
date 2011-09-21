ae00e0c88fe7cd15d8eac8829e989a893d0d657a
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2011-09-21 08:30:28 +0000

    Added EsapiTypes.h (Char is type char at the moment - let the fun begin.)

---
+++ trunk/esapi/EsapiTypes.h
@@ -0,0 +1,30 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ *
+ * @author Kevin Wall, kevin.w.wall@gmail.com
+ * @author Jeffrey Walton, noloader@gmail.com
+ * @author Daniel Amodio, dan.amodio@aspectsecurity.com
+ * @author Andrew Durkin, atdurkin@gmail.com
+ *
+ */
+
+#include <string>
+
+namespace esapi
+{
+  //typedef wchar_t Char;
+  //typedef std::wstring String;
+
+  typedef char Char;
+  typedef std::string String;
+
+  typedef std::vector<String> StringArray;
+  typedef std::list<String> StringList;
+  // typedef std::map<std::string> StringMap;
+}
