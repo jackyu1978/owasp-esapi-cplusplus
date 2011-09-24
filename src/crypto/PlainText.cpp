a7bc9d47792b07b73aa59248f2304cbf22fd8b3d
noloader <noloader@gmail.com>
2011-09-24 13:52:56 +0000

    Formatting

--- src/crypto/PlainText.cpp
+++ src/crypto/PlainText.cpp
@@ -22,70 +22,70 @@
 namespace esapi
 {
 
-PlainText::PlainText(String str) //:See catch statement below.
-{
-     /*
-     try
-     {
-     ESAPI_ASSERT2(!str.empty(), "String for PlainText cannot be null or empty.");
-     //rawBytes = toUTF8(str); //:Convert to UTF-8
-     }
-     catch()   //:Not sure what should be the catch parameters. "UnsupportedEncodingException e" gave errors.
-     {
-     //logger.error(Logger.EVENT_FAILURE, "plaintext(String) CTOR failed: Can't find UTF-8 byte-encoding!", UnsupportedEncodingException);
-     throw EncodingException(L"Can't find UTF-8 byte encoding!");
-     }
-     */
-}
+  PlainText::PlainText(String str) //:See catch statement below.
+  {
+    /*
+      try
+      {
+      ESAPI_ASSERT2(!str.empty(), "String for PlainText cannot be null or empty.");
+      //rawBytes = toUTF8(str); //:Convert to UTF-8
+      }
+      catch()   //:Not sure what should be the catch parameters. "UnsupportedEncodingException e" gave errors.
+      {
+      //logger.error(Logger.EVENT_FAILURE, "plaintext(String) CTOR failed: Can't find UTF-8 byte-encoding!", UnsupportedEncodingException);
+      throw EncodingException(L"Can't find UTF-8 byte encoding!");
+      }
+    */
+  }
 
-PlainText::PlainText(const esapi::SecureByteArray &b)
-{
-ESAPI_ASSERT2(!b.empty(), "Secure byte array representing PlainText cannot be null.");
-rawBytes = b;
-}
+  PlainText::PlainText(const esapi::SecureByteArray &b)
+  {
+    ESAPI_ASSERT2(!b.empty(), "Secure byte array representing PlainText cannot be null.");
+    rawBytes = b;
+  }
 
-PlainText::PlainText()
-{
-}
+  PlainText::PlainText()
+  {
+  }
 
-String PlainText::toString() //:Commented out for same reason as first constructor.
-{
-     /*
-     try
-     {
-     String result;
-     //result = toUni(rawBytes); //:Convert to Unicode
-     return result;
-     }
-     catch()
-     {
-     //logger.error(Logger.EVENT_FAILURE, "PlainText.toString() failed: Can't find UTF-8 byte encoding!", UnsupportedEncodingException);
-     throw EncodingException(L"Can't find UTF-8 byte encoding!");//, UnsupportedEncodingException);
-     }
-     */
-return L"";
-}
+  String PlainText::toString() //:Commented out for same reason as first constructor.
+  {
+    /*
+      try
+      {
+      String result;
+      //result = toUni(rawBytes); //:Convert to Unicode
+      return result;
+      }
+      catch()
+      {
+      //logger.error(Logger.EVENT_FAILURE, "PlainText.toString() failed: Can't find UTF-8 byte encoding!", UnsupportedEncodingException);
+      throw EncodingException(L"Can't find UTF-8 byte encoding!");//, UnsupportedEncodingException);
+      }
+    */
+    return L"";
+  }
 
-esapi::SecureByteArray PlainText::asBytes()
-{
-return rawBytes;
-}
+  esapi::SecureByteArray PlainText::asBytes()
+  {
+    return rawBytes;
+  }
 
-bool PlainText::equals(PlainText obj)
-{
-if(toString() == obj.toString())
-   return true;
-return false;
-}
+  bool PlainText::equals(PlainText obj)
+  {
+    if(toString() == obj.toString())
+      return true;
+    return false;
+  }
 
-size_t PlainText::length()
-{
-return rawBytes.length();
-}
+  size_t PlainText::length()
+  {
+    return rawBytes.length();
+  }
 
-void PlainText::overwrite()
-{
-rawBytes.clear();
-}
+  void PlainText::overwrite()
+  {
+    rawBytes.clear();
+  }
 
 } // NAMESPACE esapi
