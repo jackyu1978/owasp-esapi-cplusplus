c538c625303509c834cb876dd3bf87bef20fbea9
atdurkin <atdurkin@gmail.com>
2011-10-04 21:30:32 +0000

    Moved to initialization over assignment in CTORs.

--- esapi/crypto/CipherSpec.h
+++ esapi/crypto/CipherSpec.h
@@ -36,38 +36,38 @@
 namespace esapi
 {
 
-class ESAPI_EXPORT CipherSpec
-{
-private:
-     String cipher_xform_; //:Cipher Transformation, takes form of "ALG/MODE/PADDING"
-     unsigned int keySize_; //:The key size, IN BITS.
-     unsigned int blockSize_; //:The block size, IN BYTES.
-     SecureByteArray iv_; //:The initialization vector, NULL if not applicable
-     enum CipherTransformationComponent {ALG, MODE, PADDING}; //:Cipher transformation component. Format is ALG/MODE/PADDING.
-     void setCipherTransformation(const String& cipherXForm, bool fromCipher);
-     String getFromCipherXForm(CipherTransformationComponent component) const;
-public:
-     CipherSpec(const String& cipherXForm, unsigned int keySize, unsigned int blockSize, const SecureByteArray &iv); //:Explicitly sets everything.
-     CipherSpec(const String& cipherXForm, unsigned int keySize, unsigned int blockSize); //:Sets everything but IV.
-     CipherSpec(const String& cipherXForm, unsigned int keySize); //:Sets everything but blockSize and IV
-     CipherSpec(const String& cipherXForm, unsigned int keySize, const SecureByteArray &iv); //:Sets everything but blockSize.
-     CipherSpec(const SecureByteArray &iv); //:Sets only iv
-     CipherSpec(); //:Created because of an error in another file.
+  class ESAPI_EXPORT CipherSpec
+  {
+  private:
+    String cipher_xform_; //:Cipher Transformation, takes form of "ALG/MODE/PADDING"
+    unsigned int keySize_; //:The key size, IN BITS.
+    unsigned int blockSize_; //:The block size, IN BYTES.
+    SecureByteArray iv_; //:The initialization vector, NULL if not applicable
+    enum CipherTransformationComponent {ALG, MODE, PADDING}; //:Cipher transformation component. Format is ALG/MODE/PADDING.
+    String getFromCipherXForm(CipherTransformationComponent component) const;
+  public:
+    CipherSpec(const String& cipherXForm, unsigned int keySize, unsigned int blockSize, const SecureByteArray &iv); //:Explicitly sets everything.
+    CipherSpec(const String& cipherXForm, unsigned int keySize, unsigned int blockSize); //:Sets everything but IV.
+    CipherSpec(const String& cipherXForm, unsigned int keySize); //:Sets everything but blockSize and IV
+    CipherSpec(const String& cipherXForm, unsigned int keySize, const SecureByteArray &iv); //:Sets everything but blockSize.
+    CipherSpec(const SecureByteArray &iv); //:Sets only iv
+    CipherSpec(); //:Created because of an error in another file.
 
-     void setCipherTransformation(const String& cipherXForm);
-     String getCipherTransformation() const;
-     void setKeySize(unsigned int keySize);
-     unsigned int getKeySize() const;
-     void setBlockSize(unsigned int blockSize);
-     unsigned int getBlockSize() const;
-     String getCipherAlgorithm() const;
-     String getCipherMode() const;
-     String getPaddingScheme() const;
-     void setIV(const SecureByteArray &iv);
-     SecureByteArray getIV() const;
-     bool requiresIV() const;
-     String toString() const; //:Returns a meaningful {@code String} describing the object.
-     bool equals(const CipherSpec& obj) const;
-};
+    String verifyCipherXForm(const String& cipherXForm);
+    void setCipherTransformation(const String& cipherXForm);
+    String getCipherTransformation() const;
+    void setKeySize(unsigned int keySize);
+    unsigned int getKeySize() const;
+    void setBlockSize(unsigned int blockSize);
+    unsigned int getBlockSize() const;
+    String getCipherAlgorithm() const;
+    String getCipherMode() const;
+    String getPaddingScheme() const;
+    void setIV(const SecureByteArray &iv);
+    SecureByteArray getIV() const;
+    bool requiresIV() const;
+    String toString() const; //:Returns a meaningful {@code String} describing the object.
+    bool equals(const CipherSpec& obj) const;
+  };
 
 } // NAMESPACE esapi
