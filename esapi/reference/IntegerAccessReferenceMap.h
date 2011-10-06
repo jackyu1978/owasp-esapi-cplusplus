c68c9aebd995ecb03cb9abb3e936a4704509cf86
noloader <noloader@gmail.com>
2011-10-06 06:13:44 +0000

    Whitespace ESAPI copyright

--- esapi/reference/IntegerAccessReferenceMap.h
+++ esapi/reference/IntegerAccessReferenceMap.h
@@ -1,24 +1,30 @@
-#ifndef _IntegerAccessReferenceMap_h_
-#define _IntegerAccessReferenceMap_h_
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ */
 
-#include <string>
+#pragma once
 
+#include "EsapiCommon.h"
 #include "AccessReferenceMap.h"
 
 namespace esapi
 {
-	class ESAPI_EXPORT IntegerAccessReferenceMap : AccessReferenceMap
-	{
-	protected:
+  class ESAPI_EXPORT IntegerAccessReferenceMap : AccessReferenceMap
+    {
+    protected:
 
-		virtual String getUniqueReference() =0;
+      virtual String getUniqueReference() =0;
 
-	private:
-		int count;
+    private:
+      int count;
 
-		virtual ~IntegerAccessReferenceMap() {};
-	};
+      virtual ~IntegerAccessReferenceMap() {};
+    };
 };
-
-#endif /** _IntegerAccessReferenceMap_h_ */
 
