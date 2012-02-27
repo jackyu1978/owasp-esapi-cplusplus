19bfe13af62a6f8dbb78ba2471129af11898cf2c
noloader <noloader@gmail.com>
2012-02-27 01:29:24 +0000

    Cut in new logic for Static/Dynamic linking on Windows to appease Boost.

--- test/reference/DefaultEncoderTest.cpp
+++ test/reference/DefaultEncoderTest.cpp
@@ -11,17 +11,18 @@
  *
  */
 
-#if defined(_WIN32)
-    #if defined(STATIC_TEST)
-        // do not enable BOOST_TEST_DYN_LINK
-    #elif defined(DLL_TEST)
-        #define BOOST_TEST_DYN_LINK
-    #else
-        #error "For Windows you must define either STATIC_TEST or DLL_TEST"
-    #endif
+#include "EsapiCommon.h"
+
+#if defined(ESAPI_OS_WINDOWS_STATIC)
+// do not enable BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS_DYNAMIC)
+# define BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS)
+# error "For Windows, ESAPI_OS_WINDOWS_STATIC or ESAPI_OS_WINDOWS_DYNAMIC must be defined"
 #else
-    #define BOOST_TEST_DYN_LINK
+# define BOOST_TEST_DYN_LINK
 #endif
+
 #include <boost/test/unit_test.hpp>
 using namespace boost::unit_test;
 
