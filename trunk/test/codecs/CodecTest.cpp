04b6bb0cf3c3e054e1e0c469e07a36e7a591d792
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2012-02-27 00:48:35 +0000

    Cut in new logic for Static/Dynamic linking on Windows to appease Boost.

--- trunk/test/codecs/CodecTest.cpp
+++ trunk/test/codecs/CodecTest.cpp
@@ -10,19 +10,18 @@
  * @author Dan Amodio, dan.amodio@aspectsecurity.com
  */
 
-#include <iostream>
+#include "EsapiCommon.h"
 
-#if defined(_WIN32)
-    #if defined(STATIC_TEST)
-        // do not enable BOOST_TEST_DYN_LINK
-    #elif defined(DLL_TEST)
-        #define BOOST_TEST_DYN_LINK
-    #else
-        #error "For Windows you must define either STATIC_TEST or DLL_TEST"
-    #endif
+#if defined(ESAPI_OS_WINDOWS_STATIC)
+// do not enable BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS_DYNAMIC)
+# define BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS)
+# error "For Windows, ESAPI_OS_WINDOWS_STATIC or ESAPI_OS_WINDOWS_DYNAMIC must be defined"
 #else
-    #define BOOST_TEST_DYN_LINK
+# define BOOST_TEST_DYN_LINK
 #endif
+
 #include <boost/test/unit_test.hpp>
 using namespace boost::unit_test;
 
@@ -33,6 +32,7 @@ using esapi::String;
 #include "codecs/Codec.h"
 using esapi::Codec;
 
+#include <iostream>
 #include <string>
 #include <sstream>
 
