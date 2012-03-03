7586e753d800660cc40c9fa54cdc1adc65852680
The.jAHOLMES <The.jAHOLMES@gmail.com>
2012-03-03 19:31:24 +0000

    Added updated reference map and unit tests to new project disabled unit tests that were failing so I could run the new ones added boost library path

--- test/reference/GenericAccessReferenceMapTest.cpp
+++ test/reference/GenericAccessReferenceMapTest.cpp
@@ -11,17 +11,18 @@
  *
  */
 
-#if defined(_WIN32)
-    #if defined(STATIC_TEST)
-        // do not enable BOOST_TEST_DYN_LINK
-    #elif defined(DLL_TEST)
-        #define BOOST_TEST_DYN_LINK
-    #else
-        #error "For Windows you must define either STATIC_TEST or DLL_TEST"
-    #endif
+#include "EsapiCommon.h"
+
+#if defined(ESAPI_OS_WINDOWS_STATIC)
+// do not enable BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS_DYNAMIC)
+# define BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS)
+# error "For Windows, ESAPI_OS_WINDOWS_STATIC or ESAPI_OS_WINDOWS_DYNAMIC must be defined"
 #else
-    #define BOOST_TEST_DYN_LINK
+# define BOOST_TEST_DYN_LINK
 #endif
+
 #include <boost/test/unit_test.hpp>
 using namespace boost::unit_test;
 
@@ -30,7 +31,6 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-#include "EsapiCommon.h"
 using esapi::Char;
 using esapi::String;
 
@@ -67,9 +67,6 @@ namespace esapi
 
         virtual ~TestGenericAccessReferenceMap() {};
 
-
-    private:
-
         int count;
 
     };
@@ -77,6 +74,8 @@ namespace esapi
 
     BOOST_AUTO_TEST_CASE( GenericRefMapTest_CTORDTOR )
     {
+        BOOST_MESSAGE( "Verifying GenericAccessReferenceMap class" );
+
         TestGenericAccessReferenceMap * test_ptr_obj = new TestGenericAccessReferenceMap();
 
         // object created, good start
--- test/reference/IntegerAccessReferenceMapTest.cpp
+++ test/reference/IntegerAccessReferenceMapTest.cpp
@@ -11,17 +11,18 @@
  *
  */
 
-#if defined(_WIN32)
-    #if defined(STATIC_TEST)
-        // do not enable BOOST_TEST_DYN_LINK
-    #elif defined(DLL_TEST)
-        #define BOOST_TEST_DYN_LINK
-    #else
-        #error "For Windows you must define either STATIC_TEST or DLL_TEST"
-    #endif
+#include "EsapiCommon.h"
+
+#if defined(ESAPI_OS_WINDOWS_STATIC)
+// do not enable BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS_DYNAMIC)
+# define BOOST_TEST_DYN_LINK
+#elif defined(ESAPI_OS_WINDOWS)
+# error "For Windows, ESAPI_OS_WINDOWS_STATIC or ESAPI_OS_WINDOWS_DYNAMIC must be defined"
 #else
-    #define BOOST_TEST_DYN_LINK
+# define BOOST_TEST_DYN_LINK
 #endif
+
 #include <boost/test/unit_test.hpp>
 using namespace boost::unit_test;
 
@@ -31,7 +32,6 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-#include "EsapiCommon.h"
 using esapi::Char;
 using esapi::String;
 
@@ -48,6 +48,8 @@ namespace esapi
 
     BOOST_AUTO_TEST_CASE( IntegerRefMapTest_CtorDtor )
     {
+        BOOST_MESSAGE( "Verifying IntegerAccessReferenceMap class" );
+
        IntegerAccessReferenceMap * test_ptr_obj = new IntegerAccessReferenceMap;
 
        BOOST_CHECK_MESSAGE( test_ptr_obj != NULL, "CTOR failed to create ptr" );
