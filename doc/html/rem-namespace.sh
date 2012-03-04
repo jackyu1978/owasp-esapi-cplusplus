4de565ba3f953b5530302686d5001bb460cb01a5
noloader <noloader@gmail.com>
2012-03-04 05:06:17 +0000

    Updated comment

--- doc/html/rem-namespace.sh
+++ doc/html/rem-namespace.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# If you get Operation not permitted on Mac OS X, try the following:
+# If you get "Bad Interpreter" or "Operation not Permitted" on Mac OS X,
+# try the following:
 # xattr -d com.apple.quarantine scriptname.sh
 
 for file in ./*.html; do
