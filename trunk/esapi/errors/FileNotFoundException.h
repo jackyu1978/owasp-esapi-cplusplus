e21b613cf2e5dd257e1a16273aeaa2f2c46bc5f6
Go55am3r@gmail.com <Go55am3r@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2012-12-10 01:37:52 +0000

    Missing exception classes added.

---
+++ trunk/esapi/errors/FileNotFoundException.h
@@ -0,0 +1,42 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ */
+
+#pragma once
+
+#include "EsapiCommon.h"
+#include "errors/EnterpriseSecurityException.h"
+
+#include <stdexcept>
+#include <typeinfo>
+#include <string>
+
+namespace esapi {
+
+class ESAPI_EXPORT FileNotFoundException: public EnterpriseSecurityException {
+public:
+	explicit FileNotFoundException(const String &message) :
+		EnterpriseSecurityException(message, message) {
+	}
+	explicit FileNotFoundException(const String &userMessage,
+			const String &logMessage) :
+		EnterpriseSecurityException(userMessage, logMessage) {
+	}
+
+	explicit FileNotFoundException(const NarrowString &message) :
+		EnterpriseSecurityException(message, message) {
+	}
+	explicit FileNotFoundException(const NarrowString &userMessage,
+			const NarrowString &logMessage) :
+		EnterpriseSecurityException(userMessage, logMessage) {
+	}
+};
+
+} // NAMESPACE
+
