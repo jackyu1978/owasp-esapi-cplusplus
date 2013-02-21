9f16a45800b8c62f09fb2fd5d3592438e7064829
noloader@gmail.com <noloader@gmail.com@c04c3937-fa8f-2727-f6fa-090e764719fc>
2013-02-21 18:08:46 +0000

    Added EsapiAssert.h for Assert'ing. It will allow EsapiCommon.h to be precompiled

---
+++ trunk/esapi/EsapiAssert.h
@@ -0,0 +1,18 @@
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * http://www.owasp.org/index.php/ESAPI.
+ *
+ * Copyright (c) 2011 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ *
+ * @author Jeffrey Walton, mailto:noloader@gmail.com, Vice President of Staples
+ * @author Kevin Wall mailto:kevin.w.wall@gmail.com, Vice President of Paperclips
+ * @created 2011
+ */
+
+#include "EsapiCommon.h"
